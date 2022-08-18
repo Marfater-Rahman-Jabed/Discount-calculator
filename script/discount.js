1d9fd84e99118a68fa06f29df2b7495a9ca2c333
Marfater Rahman Jabed <marfaterrahman@gmail.com>
2022-08-18 21:52:07 +0600

    calculation of discount

---
+++ script/discount.js
@@ -0,0 +1,56 @@
+function getInformation(elementId) {
+    const getAmountElement = document.getElementById(elementId);
+    const getAmountString = getAmountElement.value;
+    // getAmountElement.value = '';
+    const Amount = parseFloat(getAmountString);
+    return Amount;
+}
+
+function GetCouponCode(elementId) {
+    const getCouponElement = document.getElementById(elementId);
+    const getCouponString = getCouponElement.value;
+    // getCouponElement.value = '';
+    return getCouponString;
+}
+
+function updateBill(elementId) {
+    const getUpdateElement = document.getElementById(elementId);
+    const getUpdate = getUpdateElement.innerText;
+    const getUpdateValue = parseFloat(getUpdate);
+    return getUpdateValue;
+}
+
+function setvalue(elementId, elementvalue) {
+    const newBill = document.getElementById(elementId);
+    newBill.innerText = elementvalue;
+}
+
+
+document.getElementById('btn-discount').addEventListener('click', function () {
+    const AmountOfBill = getInformation('input-bill');
+    const AmountOfDiscount = getInformation('input-discount');
+    const Coupon = GetCouponCode('input-coupon');
+    // console.log(AmountOfBill, AmountOfDiscount, Coupon);
+    // console.log('buttonclicked')
+
+    const totalBillPrevious = updateBill('total-bill');
+    const totalDiscount = updateBill('get-discount');
+
+
+    if (Coupon == 'JABED VAI ER FRIEND') {
+        const calculation = AmountOfBill - (AmountOfBill * (AmountOfDiscount / 100));
+        alert('Congratulations!! You get discount from our resturent..Thank You')
+        setvalue('total-bill', calculation);
+        setvalue('get-discount', (AmountOfBill * (AmountOfDiscount / 100)))
+    }
+    else {
+        const calculation = AmountOfBill;
+        alert('Sorry!!! Your Promo code Does not valid.You have to paid full amount. Thank you')
+        setvalue('total-bill', calculation);
+        setvalue('get-discount', 0)
+    }
+
+
+
+
+})
